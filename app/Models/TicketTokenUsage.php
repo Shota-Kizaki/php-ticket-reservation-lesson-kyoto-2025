6c6973f10a2c6d7675fdadea1b2b8983bec37a60
KamomeKamone <ktc2243030002@edu.kyoto-tech.ac.jp>
2025-09-19 17:53:13 +0900

    チケット消費に対応

---
+++ app/Models/TicketTokenUsage.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use App\DbConnection;
+
+
+class TicketTokenUsage
+{
+    // tokenから情報を取得
+    public static function consumeToken(string $token): array|false
+    {
+        try {
+            // $dbh = new \PDO($dsn, $db_config['user'], $db_config['pass'], $opt);
+            $dbh = DbConnection::get();
+
+            // トランザクション開始
+            $dbh->beginTransaction();
+            // プリペアドステートメント
+            $stmt = $dbh->prepare('SELECT * FROM ticket_purchases WHERE token = :token FOR UPDATE;');
+            $stmt->bindValue(':token', $token, \PDO::PARAM_STR);
+            $stmt->execute();
+            $tokenUsage = $stmt->fetch();
+            // 使用済みかどうか
+            if (false === $tokenUsage) {
+                $dbh->rollBack();
+                return false;
+            }
+            // 使用済みにする
+            $dbh = DbConnection::get();
+            // プリペアドステートメント
+            $sql = 'INSERT INTO ticket_token_usages (token, created_at, updated_at) VALUES (:token, :created_at, :updated_at);';
+            $pre = $dbh->prepare($sql);
+            $now = date('Y-m-d H:i:s');
+            $pre->bindValue(':token', $token, \PDO::PARAM_STR);
+            $pre->bindValue(':created_at', $now, \PDO::PARAM_STR);
+            $pre->bindValue(':updated_at', $now, \PDO::PARAM_STR);
+            $res = $pre->execute();
+            // コミット
+            $dbh->commit();
+if 
+        } catch (\PDOException $e) {
+            // XXX 暫定: 本来はlogに出力する & エラーページを出力する
+            echo $e->getMessage();
+            exit;
+        }
+        return $datum;
+    }
+    
+    public static function markAsUsed(string $token): bool
+    {
+        try {
+            // $dbh = new \PDO($dsn, $db_config['user'], $db_config['pass'], $opt);
+            
+            $res = $pre->execute();
+        } catch (\PDOException $e) {
+            // XXX 暫定: 本来はlogに出力する & エラーページを出力する
+            echo $e->getMessage();
+            exit;
+        }
+        return $res;
+    }
+}
--- public/entry.php
+++ public/entry.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../app/initialize.php';
 
-use app\DbConnection;
+use App\DbConnection;
+use App\Models\TicketPurchase;
+use App\Models\TicketTokenUsage;
 
 // tokenの把握
 if ('' === ($token = strval($_GET['token'] ?? ''))) {
@@ -14,35 +16,18 @@ if ('' === ($token = strval($_GET['token'] ?? ''))) {
 }
 
 /* tokenの確認 */
-// DB接続情報
-try {
-    $dbh = DbConnection::get();
-} catch (\PDOException $e) {
-    // XXX 暫定: 本来はlogに出力する & エラーページを出力する
-    echo $e->getMessage();
-    exit;
-}
-try {
-    // データの取得
-    // プリペアドステートメント
-    $sql = 'SELECT * FROM ticket_purchases WHERE token = :token;';
-    $pre = $dbh->prepare($sql);
-    //
-    $pre->bindValue(':token', $token, PDO::PARAM_STR);
-    //
-    $pre->execute();
-    $datum = $pre->fetch();
-} catch (\PDOException $e) {
-    // XXX 暫定: 本来はlogに出力する & エラーページを出力する
-    echo $e->getMessage();
-    exit;
-}
+$datum = TicketPurchase::getByToken($token);
 
 // なかったらエラー出力
 if (false === $datum) {
     echo $twig->render('entry_error.twig');
     exit;
+} else {
+    // tokenがあったら、使用履歴を登録する
+    TicketTokenUsage::consumeToken($token);
 }
+
+// 使用
 
 // あったら最低限の情報表示
 echo $twig->render('entry.twig', [
